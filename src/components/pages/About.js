fc7a4e957dd4afadfba59868a0a31ed53f4e6a81
daltonlhazzard <daltonlhazzard@gmail.com>
2019-07-13 11:43:02 -0400

    comments added

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,9 @@ function About() {
     return (
         <React.Fragment>
             <h1>About</h1>
-            <p>This is the TodoList app v1.0.0 It is part of a React crash course</p>
+            <p>This TodoList app v1.0.0 utilizes React.JS. It renders various components that allow you to: create a todo, delete a todo, and change the state of a todo. </p>
+            Terminal commands during build: npx create-react-app ., cd to-do, npm start.
+            
         </React.Fragment>
     )
 }
